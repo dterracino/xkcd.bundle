d5394246f0e6a97dfe278964cf6dfcaafda39dc4
sengian <sengian1@gmail.com>
2012-10-15 23:11:23 +0200

    Fixed XKCD plugin + v2 framework. Still some fine tuning to do to be perfectly satisfactory.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,34 +1,83 @@
-import urlparse
+from urlparse import urljoin
+from math import ceil
 
-PLUGIN_PREFIX   = "/photos/xkcd"
+NAME    = 'xkcd'
+ART      = 'art-default.jpg'
+ICON     = 'icon-default.png'
 CACHE_1YEAR = 365 * CACHE_1DAY
 ####################################################################################################
 def Start():
-  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, "xkcd", "icon-default.png", "art-default.jpg")
-  Plugin.AddViewGroup("_List", viewMode="List", mediaType="items")
+    # Set the default ObjectContainer attributes
+    ObjectContainer.art = R(ART)
+    ObjectContainer.title1 = NAME
+
+    # Default icons for DirectoryObject
+    DirectoryObject.thumb = R(ICON)
+    DirectoryObject.art = R(ART)
+
+    # Set the default cache time
+    HTTP.CacheTime = CACHE_1HOUR
 
 ####################################################################################################
+@handler('/photos/xkcd', NAME, art = ART, thumb = ICON)
+def XKCDMenu():
+    oc = ObjectContainer()
+    StripsList = GetStripList()
+    nb_images = int(Prefs['nbimgindir'])
+    
+    if len(StripsList) < nb_images:
+        #will never happen
+        imgXpath = '//div[@id="comic"]//img'
+        for comicURL in GetStripList():
+            imgs = HTML.ElementFromURL(comicURL, cacheTime=CACHE_1YEAR).xpath(imgXpath)[0]
+            img = imgs.get('src')
+            resume = imgs.get('title')
+            titre = imgs.get('alt')
+            oc.add(PhotoObject(url=comicURL, title=titre, thumb=Callback(GetIcon, stripURL=comicURL), summary=resume))
+    else:
+        #Create subdirectories containing a fixed number of images
+        nbdir = ceil(float(len(StripsList))/nb_images)
+        for i in xrange(1,nbdir+1):
+            firstelt = (i-1)*nb_images
+            lastelt = i*nb_images
+            if i == nbdir:
+                lastelt = len(StripsList)
+            name = '%d - %d' % (firstelt+1, lastelt)
+            oc.add(DirectoryObject(key = Callback(StripDirectory, stripsstart=firstelt, stripsstop=lastelt),
+                    title=name, thumb =Callback(GetIcon, stripURL=StripsList[firstelt])))
+    return oc
 
-def MainMenu():
-	dirTitle = 'XKCD'
-	archiveURL = 'http://xkcd.com/archive/'
-	archiveXPath = '//div[@class="s"]/h1/following-sibling::a'
-	dir = MediaContainer(title1=dirTitle)
-	
-	for comic in HTML.ElementFromURL(archiveURL).xpath(archiveXPath):
-		comicURL = urlparse.urljoin(archiveURL, comic.get('href'))
-		try:
-			title = comic.xpath('./font')[0].text
-		except:
-			title = comic.text
-		dir.Append(Function(PhotoItem(GetPhotoItem, title=title, thumb=Function(GetPhotoItem, url=comicURL)), url=comicURL))
-	return dir
+####################################################################################################
+@route('/photos/xkcd/stripdir')
+def StripDirectory(stripsstart, stripsstop, sender = None):
+    imgXpath = '//div[@id="comic"]//img'
+    oc = ObjectContainer()
 
-def GetPhotoItem(url, sender=None):
-	xpaths = ['//div[@class="s"]/a/img', '//div[@class="s"]/img']
-	page = HTML.ElementFromURL(url, cacheTime=CACHE_1YEAR)
-	for xpath in xpaths:
-		imgs = page.xpath(xpath)
-		if len(imgs) != 0:
-			img = imgs[0].get('src')
-			return Redirect(img)
+    for comicURL in GetStripList()[int(stripsstart):int(stripsstop)]:
+        imgs = HTML.ElementFromURL(comicURL, cacheTime=CACHE_1YEAR).xpath(imgXpath)[0]
+        img = imgs.get('src')
+        resume = imgs.get('title')
+        titre = imgs.get('alt')
+        oc.add(PhotoObject(url=comicURL, title=titre, thumb=Callback(GetIcon, stripURL=comicURL), summary=resume))
+    return oc
+
+####################################################################################################
+@route('/photos/xkcd/geticon')
+def GetIcon(stripURL, sender=None):
+    imgXpath = '//div[@id="comic"]//img'
+    imgs = HTML.ElementFromURL(stripURL, cacheTime=CACHE_1YEAR).xpath(imgXpath)
+    img = R(ICON)
+    if len(imgs):
+        img = imgs[0].get('src')
+    return Redirect(img)
+
+####################################################################################################
+@route('/photos/xkcd/getstriplist')
+def GetStripList(sender=None):
+    archiveURL = 'http://xkcd.com/archive/'
+    archiveXPath = '//div[@id="middleContainer"]/a'
+    # Get all the elements needed to determine the number of entries and how to sectionize them
+    StripsMainPage = HTML.ElementFromURL(archiveURL).xpath(archiveXPath)
+    StripsList = [urljoin(archiveURL, comic.get('href')) for comic in StripsMainPage]
+    StripsList.reverse()
+    return StripsList
